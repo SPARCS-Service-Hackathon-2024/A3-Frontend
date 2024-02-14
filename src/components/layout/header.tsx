b61165715c88c575273fd024e1953739ef84ea2e
r4bb1t <r4bb1t.github@gmail.com>
2024-02-14 16:51:44 +0900

    refactor: separate header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-base-100 flex h-16 items-center px-8 font-bold">
+    <header className="bg-base-100 absolute inset-x-0 top-0 flex h-16 items-center px-8 font-bold">
       Life book
     </header>
   );
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,9 +4,9 @@ import Header from "./header";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="bg-base-200 flex h-full w-full flex-col items-center">
-      <div className="flex h-full min-h-screen w-full max-w-lg flex-col bg-white shadow-xl">
+      <div className="relative flex h-screen w-full max-w-lg flex-col bg-white shadow-xl">
         <Header />
-        <main className="h-full w-full">{children}</main>
+        <main className="h-full w-full overflow-y-auto">{children}</main>
       </div>
     </div>
   );
