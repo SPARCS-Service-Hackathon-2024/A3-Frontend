c279b8885e60fc9116a2e11f9b2d64506ff1d5da
r4bb1t <r4bb1t.github@gmail.com>
2024-02-14 15:33:39 +0900

    refactor: separate header and make serif font to default

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-base-100 flex h-16 items-center px-8 font-bold">
+      Life book
+    </header>
+  );
+}
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,11 @@
 import { ReactNode } from "react";
+import Header from "./header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="bg-base-200 flex h-full w-full flex-col items-center">
       <div className="flex h-full min-h-screen w-full max-w-lg flex-col bg-white shadow-xl">
-        <header className="bg-base-100 flex h-16 items-center px-8 font-bold">
-          A3 frontend
-        </header>
+        <Header />
         <main className="h-full w-full">{children}</main>
       </div>
     </div>
