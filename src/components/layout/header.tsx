b2bf84afecf183c1b31672a465738aea35c63b2b
r4bb1t <r4bb1t.github@gmail.com>
2024-02-15 15:00:35 +0900

    chore: change title

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="absolute inset-x-0 top-0 flex h-16 items-center bg-base-100 px-8 font-bold">
-      Life book
+      인생 기록
     </header>
   );
 }
