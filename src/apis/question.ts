1122a3cb3269b77f151bcf73e62daf1f51f3c560
r4bb1t <r4bb1t.github@gmail.com>
2024-02-15 20:13:44 +0900

    feat: add skip api

--- src/apis/question.ts
+++ src/apis/question.ts
@@ -1,8 +1,20 @@
 import { QuestionType } from "../types/question";
 
-export const getQuestion = async ({ questionId }: { questionId: number }) => {
+export const getQuestion = async ({
+  questionId,
+  token,
+}: {
+  questionId: number;
+  token: string;
+}) => {
   const result = await fetch(
     `${import.meta.env.VITE_API_URL}/question/${questionId}`,
+    {
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
   );
   const json: QuestionType = await result.json();
   return json;
@@ -11,9 +23,11 @@ export const getQuestion = async ({ questionId }: { questionId: number }) => {
 export const submitAnswer = async ({
   questionId,
   answer,
+  token,
 }: {
   questionId: number;
   answer: string;
+  token: string;
 }) => {
   const result = await fetch(
     `${import.meta.env.VITE_API_URL}/question/${questionId}`,
@@ -21,8 +35,30 @@ export const submitAnswer = async ({
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
+      credentials: "include",
       body: JSON.stringify({ answer }),
+    },
+  );
+  const json: { question_id: number } = await result.json();
+  return json;
+};
+
+export const skipQuestion = async ({
+  questionId,
+  token,
+}: {
+  questionId: number;
+  token: string;
+}) => {
+  const result = await fetch(
+    `${import.meta.env.VITE_API_URL}/question/${questionId}/skip`,
+    {
+      credentials: "include",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     },
   );
   const json: { question_id: number } = await result.json();
