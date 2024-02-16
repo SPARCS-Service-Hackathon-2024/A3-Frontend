0a8f130d79fd58fba92b107041527e109d629f0d
r4bb1t <r4bb1t.github@gmail.com>
2024-02-16 10:06:51 +0900

    feat: add link to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
 
   return (
     <header className="absolute inset-x-0 top-0 flex h-16 items-center justify-between bg-base-100 px-8 font-bold">
-      인생 기록
+      <a href="/">인생 기록</a>
       {user && <div>{user.name} 님</div>}
     </header>
   );
