c46e8a33fa6002622322841911a1ed0dd10afef3
r-4bb1t <r4bb1t.github@gmail.com>
2025-02-20 00:10:19 +0900

    fix: fix allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   server: {
-    allowedHosts: ["13ad-58-226-33-91.ngrok-free.app"],
+    allowedHosts: ["life-book-tau.vercel.app", "localhost"],
   },
   plugins: [
     react(),
