a4613d37db25d375029c49e11d1f8f04e5f353c5
r4bb1t <r4bb1t.github@gmail.com>
2025-02-20 11:52:34 +0900

    style: add z-index to header for proper layering

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   const { user } = useUser();
 
   return (
-    <header className="absolute inset-x-0 top-0 flex h-16 items-center justify-between bg-base-100 px-8 font-bold">
+    <header className="absolute inset-x-0 top-0 z-10 flex h-16 items-center justify-between bg-base-100 px-8 font-bold">
       <a href="/">인생 기록</a>
       {user && <div>{user.name} 님</div>}
     </header>
